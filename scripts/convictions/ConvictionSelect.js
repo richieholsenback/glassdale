a9b3dbdf0f6cbc4416062289f5595a324ad4d61d
Richie Holsenback <richieholsenback@gmail.com>
2020-08-31 15:38:35 -0500

    cleaned up formatting

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -4,29 +4,23 @@
  */
 import { useConvictions } from "./ConvictionProvider.js"
 
-// Get a reference to the DOM element where the <select> will be rendered
 const contentTarget = document.querySelector(".filters__crime")
 
 export const ConvictionSelect = () => {
-    // Get all convictions from application state
+    
     const convictions = useConvictions()
 
     const render = (convictionsCollection) => {
-        /*
-            Use interpolation here to invoke the map() method on
-            the convictionsCollection to generate the option elements.
-            Look back at the example provided above.
-        */
-        
-    contentTarget.innerHTML = `
+
+        contentTarget.innerHTML = `
             <select class="dropdown" id="crimeSelect">
                 <option value="0">Please select a crime...</option>
                 ${
-                    convictionsCollection.map(convictionObj => {
-                        const convictionName = convictionObj.name
-                        return `<option>${convictionName}</option>`
-                    })
-                }
+            convictionsCollection.map(convictionObj => {
+                const convictionName = convictionObj.name
+                return `<option>${convictionName}</option>`
+            })
+            }
             </select>
         `
     }
